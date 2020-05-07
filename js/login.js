cf850b1543f85e956c7e1f98eaeea2b31ff740b3
Laurentiu Meghesan <laurentiu.meghesan@gmail.com>
2020-05-07 20:47:30 +0300

    changed the name of createProfile method to createUser and then create profile associated with user method

--- js/login.js
+++ js/login.js
@@ -1,10 +1,15 @@
 var patientId = null;
 var doctorID = null;
+var userId = null;
+var firstName = null;
+var lastName = null;
+var phoneNumber = null;
+var birthDate = null;
 
 window.Login = {
     API_URL: "http://localhost:8084",
 
-    createProfile: function () {
+    createUser: function () {
 
         let userName = $('#username-signup').val();
         let email = $('#email-signup').val();
@@ -22,9 +27,43 @@ window.Login = {
             contentType: "application/json",
             data: JSON.stringify(requestBody)
         }).done(function (event) {
-            alert("Profile created")
-        })
+            alert("User created. Now we need a few more information to complete your profile.");
+            userId = event.id;
 
+            firstName = prompt("Please enter your First Name:");
+            lastName = prompt("Please enter your Last Name:");
+            phoneNumber = prompt("Now enter your Phone Number:", "Ex: 074xxxxxxx");
+            birthDate = prompt("Enter your birthday:", "Ex: MM.DD.YYYY");
+
+            Login.createProfile();
+            alert("Profile Updated! Now try to Sign In.");
+            location.reload(true)
+        });
+    },
+
+    createProfile: function () {
+
+        let birthday = new Date(birthDate);
+        birthday.setDate(birthday.getDate() + 1);
+
+        let requestBody = {
+            profileId: userId,
+            firstName: firstName,
+            lastName: lastName,
+            phoneNumber: phoneNumber,
+            birthDate: birthday
+        };
+
+        console.log(requestBody);
+
+        $.ajax({
+            url: Login.API_URL + "/patients",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+
+        })
     },
 
     bindEvents: function () {
@@ -38,7 +77,7 @@ window.Login = {
                 alert("Password did not match! Try again.");
                 location.reload(true)
             } else {
-                Login.createProfile();
+                Login.createUser();
             }
         })
     }
