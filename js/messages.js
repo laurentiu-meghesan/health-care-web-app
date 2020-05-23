e9c4db78287d765328337a923463c67d8663c55f
Laurentiu Meghesan <laurentiu.meghesan@gmail.com>
2020-05-23 04:26:01 +0300

    messaging - part 2

--- js/messages.js
+++ js/messages.js
@@ -8,30 +8,82 @@ function closeForm() {
 
 window.Messages = {
 
-    API_BASE_URL: "http://localhost:8084",
+    API_BASE_URL: "http://localhost:8084/chats/",
 
+    createMessage: function () {
 
-    getMessages: function () {
+        let patientId = sessionStorage.getItem("loggedUserId");
+        let currentDate = Date.now();
+
+        let messageSent = $("#textarea-message").val();
+
+        let requestBody = {
+
+            doctorId: 7,
+            messageDate: new Date(currentDate),
+            messageReceived: "",
+            messageSent: messageSent,
+            patientId: patientId
+        };
+
         $.ajax({
             url: Messages.API_BASE_URL,
-            method: "GET"
-        }).done(function (response) {
-
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function (event) {
+            console.log(messageSent, event);
+            Messages.getMessages();
         })
     },
 
-    getMessageRow: function (message) {
-        return `<div class="container">
-    <img src="/w3images/bandmember.jpg" alt="Avatar">
-    <p>message</p>
-<span class="time-right">11:00</span>
-</div>
+    getMessages: function () {
 
-<div class="container darker">
-    <img src="/w3images/avatar_g2.jpg" alt="Avatar" class="right">
-    <p>Hey! I'm fine. Thanks for asking!</p>
-<span class="time-left">11:01</span>
-</div>`
+        let patientId = sessionStorage.getItem("loggedUserId");
+        userIsLoggedIn = sessionStorage.getItem("userIsLoggedIn");
+
+        $.ajax({
+            url: Messages.API_BASE_URL + "/patientId=" + patientId
+        }).done(function (response) {
+            console.log(response);
+
+            Messages.displayMessages(response.content);
+        })
     }
+    ,
 
+    displayMessages: function (messages) {
+        let messagesHtml = '';
+
+        messages.forEach(messageSent => messagesHtml += Messages.getHtmlForMessageSent(messageSent));
+        messages.forEach(messageReceived => messagesHtml += Messages.getHtmlForMessageReceived(messageReceived));
+
+        $('.messages-class .container-chat').html(messagesHtml);
+    }
+    ,
+
+    getHtmlForMessageSent: function (message) {
+        return `<div class="container-chat">
+        <img src="img/icon/icon-1.png" alt="Avatar">
+        <p>${message.messageSent}</p>
+        <span class="time-right">${message.messageDate}</span>
+    </div>`;
+
+    },
+
+    getHtmlForMessageReceived: function (message) {
+        return `<div class="container-chat darker-chat">
+        <img src="img/icon/icon-6.png" alt="Avatar" class="right">
+        <p>${message.messageReceived}</p>
+        <span class="time-left">${message.messageDate}</span>
+    </div>`;
+    },
+
+    bindEvents: function () {
+        $('#message-submit').click(function () {
+            Messages.createMessage();
+        })
+    }
 };
+Messages.getMessages();
+Messages.bindEvents();
